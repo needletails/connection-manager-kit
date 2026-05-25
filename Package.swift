cb648f5062e026ba2fe5e991ee4eaa61ee38c654
Universal Echo <universalecho@Universals-MacBook-Pro.local>
2026-05-25 13:55:15 +0800

    Fixed missing dependency

--- Package.swift
+++ Package.swift
@@ -35,6 +35,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
                 .product(name: "NIO", package: "swift-nio"),
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
                 .product(name: "NIOWebSocket", package: "swift-nio"),
                 .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
@@ -47,6 +48,7 @@ let package = Package(
             name: "ConnectionManagerKitTests",
             dependencies: [
                 "ConnectionManagerKit",
+                .product(name: "NIOFoundationCompat", package: "swift-nio"),
                 .product(name: "NIOHTTP1", package: "swift-nio"),
                 .product(name: "NIOWebSocket", package: "swift-nio"),
                 .product(name: "NIOExtras", package: "swift-nio-extras"),
